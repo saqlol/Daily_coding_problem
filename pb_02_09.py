ab387c5086a4e40b103a9cb5359b231e37c5f30e
saqlol <80032485+saqlol@users.noreply.github.com>
2021-09-03 12:01:29 +0200

    Add files via upload

---
+++ pb_02_09.py
@@ -0,0 +1,14 @@
+"""
+Given an N by N matrix, rotate it by 90 degrees clockwise.
+"""
+matrix = [[1,2],[3,4],[5,6]]
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+matrix_rotated = []
+temp = []
+for i in range(len(matrix[1])):
+    for j in range(len(matrix)):
+        temp.append(matrix[j][i])
+    matrix_rotated.append(temp[::-1])
+    temp = []
+
+print(matrix_rotated)
